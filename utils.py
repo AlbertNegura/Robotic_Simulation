45662b169c69bf77e75952852cc7affcbf24ec6d
Albert Negura <Albert.Negura@Gmail.com>
2021-02-09 16:59:20 +0100

    Rotation + Distance between two points

--- utils.py
+++ utils.py
@@ -15,9 +15,27 @@ def angle(v1,v2):
 def rotate(origin, point_of_rotation, angle_of_rotation):
     """
     Returns the coordinates of a 2D vector after it has been rotated.
-    :param origin: point of origin
-    :param point_of_rotation: point about which to rotate the object - can be point of origin to make object spin in place
+    :param origin: point of origin as a [x,y] numpy array
+    :param point_of_rotation: point about which to rotate the object as a [x,y] numpy array - can be point of origin to make object spin in place
     :param angle_of_rotation: angle about which to rotate the object
-    :return: the new point
+    :return: the new point as a numpy array
     """
-    pass
+    x = origin[0]
+    y = origin[1]
+
+    dx = point_of_rotation[0]
+    dy = point_of_rotation[1]
+
+    rx = x + np.cos(angle_of_rotation) * (dx-x) - np.sin(angle_of_rotation) * (dy - y)
+    ry = x + np.sin(angle_of_rotation) * (dx-x) - np.cos(angle_of_rotation) * (dy - y)
+
+    return np.array([rx, ry])
+
+def distance_between(a,b):
+    """
+    Returns the absolute distance between two points.
+    :param a: the first point as a [x,y] numpy array
+    :param b: the second point as a [x,y] numpy array
+    :return: the absolute distance between the two points
+    """
+    return np.linalg.norm(a-b)
