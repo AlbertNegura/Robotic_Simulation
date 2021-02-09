62e26c280056933ec53b256a03be54521831ccc7
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-02-09 20:31:53 +0100

    angular velocity correction

--- motion.py
+++ motion.py
@@ -2,22 +2,26 @@ import numpy as np
 
 def Step(Vr,Vl,l,x0,y0,theta):
     # we asume delta*t = 1 ?
-    w = omega(Vr,Vl,l)
+    w = 2*np.pi*omega(Vr,Vl,l)
     Icc_x,Icc_y = ICC(x0,y0,theta,Vl,Vr,l)
 
     x = np.cos(w)*(x0 - Icc_x) - np.sin(w)*(y0-Icc_y) + Icc_x
     y = np.sin(w)*(x0 - Icc_x) + np.cos(w)*(y0-Icc_y) + Icc_y
-    Theta = theta + w
+    Theta = theta + omega(Vr,Vl,l)
 
     return x,y,Theta
 
 def ICC(x0,y0,theta,Vl,Vr,l):
-    x = x0 - R(Vl,Vr,l)*np.sin(theta)
-    y = y0 + R(Vl,Vr,l)*np.cos(theta)
+    #theta in degrees
+    x = x0 - R(Vl,Vr,l)*np.sin(np.deg2rad(theta))
+    y = y0 + R(Vl,Vr,l)*np.cos(np.deg2rad(theta))
     return x,y 
 
 def R(Vr,Vl,l):
     return (l/2)*((Vr+Vl)/(Vr-Vl))
 
 def omega(Vr,Vl,l):
+    # s^-1
     return (Vr-Vl)/l
+
+print(Step(30,3,0.2,1,2,90))
