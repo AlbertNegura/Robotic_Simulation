b59d355331fbe446428d34371f67ac23787037cb
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-02-24 00:22:42 +0100

    RNN

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -1,8 +1,8 @@
 import numpy as np
+import copy
 import utils
 
-# A RNN MUST BE IMPLEMENTED. This is a simple feedforward neural network
-class NeuralNetwork:
+class RNN:
     """
     Output: Vr and Vl values with a range of [0, 1].
     1 means maximum rotation speed in one direction
@@ -10,28 +10,42 @@ class NeuralNetwork:
     0.5 means no motion in the corresponding wheel
     """
 
-    def __init__(self, x, y):
-        # 2-layer neural network
-        self.n_hidden = 4  # 4-neuron hidden layer
+    def __init__(self, x, y, input_dim=12, hidden_dim=4, output_dim=2):
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
         self.input = x
-        self.weights1 = np.random.rand(self.input.shape[1], self.n_hidden)
-        self.weights2 = np.random.rand(self.n_hidden, 2)
         self.y = y
         self.output = np.zeros(self.y.shape)
 
+        # nn weights
+        self.synapse_0 = 2*np.random.random((input_dim, hidden_dim)) - 1
+        self.synapse_1 = 2*np.random.random((hidden_dim, output_dim)) - 1
+        self.synapse_h = 2*np.random.random((hidden_dim, hidden_dim)) - 1
+
+        # initialize previous step layer values
+        self.layer_1_values = list()
+        self.layer_1_values.append(np.zeros(hidden_dim))
+
     def feedforward(self):
-        # We might want to use ReLU, Softmax, TanH, Step activation functions instead
-        self.layer1 = utils.sigmoid(np.dot(self.input, self.weights1))
-        self.output = utils.sigmoid(np.dot(self.layer1, self.weights2))
+        # hidden layer (input ~+ prev_hidden)
+        self.layer_1 = utils.sigmoid(np.dot(self.input, self.synapse_0) + np.dot(self.layer_1_values[-1], self.synapse_h))
+
+        # output layer
+        self.output = utils.sigmoid(np.dot(self.layer_1, self.synapse_1))
+
+        # store hidden layer so we can use it in the next time step
+        self.layer_1_values.append(copy.deepcopy(self.layer_1))
 
     def update_weights(self):
         # use fitness
-        self.weights1 = None
-        self.weights2 = None
+        self.synapse_0 = None
+        self.synapse_1 = None
+        self.synapse_h = None
 
-"""input = []
+input = []
 for i in range(12):
     input.append(100)
 x = np.array([input])
-nn = NeuralNetwork(x, np.array([[0,0]]))
-nn.feedforward()"""
+nn = RNN(x, np.array([[0,0]]))
+nn.feedforward()
