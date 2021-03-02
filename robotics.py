db0ac549763d278b5894a3c8a4a8bbfe15b8b2a6
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-03-02 19:10:56 +0100

    Collision parameter in robot

--- robotics.py
+++ robotics.py
@@ -26,6 +26,7 @@ class Robot:
     velocity = [0, 0]
     force = 0
     grid_covered = []
+    collisions = 0
 
     position_history = []
     orientation_history = []
@@ -93,6 +94,7 @@ class Robot:
                                                                                              self.orientation)
                 if is_intersection:
                     new_position = new_P
+                    self.collisions += 1
                     #update = True
             # determine new position after accounting for parallel velocity component
             new_position = [new_position[0]+self.force*np.cos(np.radians(self.orientation)),new_position[1]+self.force*np.sin(np.radians(self.orientation))]  # utils.rotate(self.position, self.position+[self.velocity_left/2+self.velocity_right/2],np.radians(self.orientation))
@@ -127,6 +129,7 @@ class Robot:
                                                                                              self.orientation)
                 if is_intersection:
                     new_position = new_P
+                    self.collisions += 1
                     #update = True
             collisions = []
             # determine new position after accounting for parallel velocity component
