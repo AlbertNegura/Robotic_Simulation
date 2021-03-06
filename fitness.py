4942d9ea7dfdaaae2ed5d9a926b770124785f1c3
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-03-06 13:47:31 +0100

    Cleaning focus fitness

--- fitness.py
+++ fitness.py
@@ -5,9 +5,9 @@ def fitness(total_area, num_c, sensor_values):
     :param num_c: minimise number of collisions
     :param sensor_values: Average value of the robot's sensors
     """
-    a = 10 # to be determined manually
-    b = -100
-    c = -1
+    a = 100 # to be determined manually
+    b = -10
+    c = 0
     sensor_values = sensor_values/1000
     total_area = total_area*10
 
