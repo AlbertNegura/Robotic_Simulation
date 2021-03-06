83e321b088609e8ec5e4cf3fe401c26d19896b3e
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-03-06 14:46:43 +0100

    Sensor value fix

--- fitness.py
+++ fitness.py
@@ -5,10 +5,8 @@ def fitness(total_area, num_c, sensor_values):
     :param num_c: minimise number of collisions
     :param sensor_values: Average value of the robot's sensors
     """
-    a = 100 # to be determined manually
+    a = 1000 # to be determined manually
     b = -10
-    c = 1
-    sensor_values = sensor_values/1000
-    total_area = total_area*10
+    c = -1
 
     return a*total_area + b*num_c + c*sensor_values
