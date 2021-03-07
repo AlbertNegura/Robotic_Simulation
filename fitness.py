8a25140548e7e83e9146d9fc22a95381e34a2c4c
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-03-07 06:45:53 +0100

    Better fitness?

--- fitness.py
+++ fitness.py
@@ -5,8 +5,8 @@ def fitness(total_area, num_c, sensor_values):
     :param num_c: minimise number of collisions
     :param sensor_values: Average value of the robot's sensors
     """
-    a = 1000 # to be determined manually
-    b = -10
-    c = -1
+    a = 2 # to be determined manually
+    b = -2
+    c = 0
 
     return a*total_area + b*num_c + c*sensor_values
