233d6e4bd07c3cf0da5e7ec6e17b038b02e9fa14
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-03-07 15:31:48 +0100

    fitness adjustment

--- fitness.py
+++ fitness.py
@@ -5,8 +5,9 @@ def fitness(total_area, num_c, sensor_values):
     :param num_c: minimise number of collisions
     :param sensor_values: Average value of the robot's sensors
     """
-    a = 2 # to be determined manually
-    b = -2
-    c = 0
-
-    return a*total_area + b*num_c + c*sensor_values
+    a = 2000 # to be determined manually
+    b = -100
+    c = -0.01
+    f = a*total_area + b*num_c + c*sensor_values
+    if(f<0): f=0
+    return f
