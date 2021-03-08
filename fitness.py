5173b955c028dbc62ff072ad3a2a45b8a830e96c
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-03-08 17:56:30 +0100

    Load fitness values from config

--- fitness.py
+++ fitness.py
@@ -1,13 +1,14 @@
 import numpy as np
+from config import *
 def fitness(total_area, num_c, sensor_values):
     """
     :param total_area: maximise total area covered (from the robot's self.position_history)
     :param num_c: minimise number of collisions
     :param sensor_values: Average value of the robot's sensors
     """
-    a = 2000 # to be determined manually
-    b = -2000
-    c = -1
+    a = DIRT_VALUE # to be determined manually
+    b = COLLISION_VALUE
+    c = SENSOR_VALUE
     f = a*total_area + b*num_c + c*sensor_values
     if(f<0): f=0
     return f/1000
